113c340fd520eaec91d515c6a37cdb1beb8b1923
Julio Gomes Pena <juliogomespena@gmail.com>
2024-11-20 11:33:34 -0300

    Update debug and exception module

---
+++ MS Foundational C#/Challenge22.cs
@@ -0,0 +1,42 @@
+﻿namespace MS_Foundational_C_
+{
+    internal class Challenge22
+    {
+        public static void Challenge()
+        {
+            try
+            {
+                Process1();
+            }
+            catch
+            {
+                Console.WriteLine("An exception has occurred");
+            }
+
+            Console.WriteLine("Exit program");
+
+            static void Process1()
+            {
+                try
+                { 
+                    WriteMessage(); 
+                }
+                catch
+                {
+                    Console.WriteLine("Caught in process1");
+                }
+            }
+
+            static void WriteMessage()
+            {
+                double float1 = 3000.0;
+                double float2 = 0.0;
+                int number1 = 3000;
+                int number2 = 0;
+
+                Console.WriteLine(float1 / float2);
+                Console.WriteLine(number1 / number2);
+            }
+        }
+    }
+}
--- MS Foundational C#/Program.cs
+++ MS Foundational C#/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Challenge21.Challenge();
+            ChallengeProject04.Challenge();
         }
     }
 }
